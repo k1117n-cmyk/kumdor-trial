ea056c71d8b78cc0d643cd2960332bc8db3cab0c
noi <pi@pcfan.net>
2026-08-01 19:37:17 +0900

    Remove blind trait from stage three

--- include/game.h
+++ include/game.h
@@ -10,7 +10,7 @@
 #define EXP_TO_LEVEL_UP 10
 #define MAX_STAGE_COUNT 14
 #define POISON_TURN_LIMIT 3
-#define GAME_VERSION "v0.8.1"
+#define GAME_VERSION "v0.8.2"
 #define SAVE_FILE "kumdor_save.txt"
 #define SAVE_COMMAND ":save"
 #define QUIT_COMMAND ":quit"
